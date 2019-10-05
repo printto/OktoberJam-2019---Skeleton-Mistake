c74402028afc674a19621a15e62646751d334afc
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2019-10-05 12:01:07 +0300

    Added testing ingredients and recipes

---
+++ Assets/_Scripts/Recipes/RecipeBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Recipe", menuName = "Recipe")]
+public class RecipeBase : ScriptableObject
+{
+
+    [Header("Recipe variables")]
+    public GameObject successSprite;
+    public IngredientBase[] recipeCode;
+    public string creatureName;
+    public string description;
+
+    public string getCode()
+    {
+        string temp = "";
+        foreach(IngredientBase ingre in recipeCode)
+        {
+            temp += ingre.ingredientName;
+        }
+        return temp;
+    }
+
+}
