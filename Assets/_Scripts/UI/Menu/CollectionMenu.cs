aa65383a28d3939c8284e3b17658f449b9245b38
Menkul Totsapon TIX18 <totsaponmenkul@kamk.fi>
2019-10-06 02:14:23 +0300

    asd

---
+++ Assets/_Scripts/UI/Menu/CollectionMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionMenu : IMenu
+{
+    public Transform UITransform;
+    public Transform BackButtonTransform;
+    public CollectionMenu(Transform parentMenu)
+    {
+        UITransform = parentMenu.Find("Canvas/CollectionMenu");
+        InitBackButton();
+    }
+    public void Show()
+    {
+        UITransform.gameObject.SetActive(true);
+    }
+
+    public void Back()
+    {
+        UITransform.gameObject.SetActive(false);
+    }
+
+    private void InitBackButton()
+    {
+        BackButtonTransform = UITransform.Find("BackButton");
+        Button backButton = BackButtonTransform.GetComponent<Button>();
+        backButton.onClick.AddListener(() => Back());
+    }
+
+
+
+
+}
