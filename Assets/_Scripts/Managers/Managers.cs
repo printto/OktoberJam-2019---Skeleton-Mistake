a8253f41b6c958ae86aa9b1cf03138c210f95eaf
Menkul Totsapon TIX18 <totsaponmenkul@kamk.fi>
2019-10-05 22:48:24 +0300

    add particle

--- Assets/_Scripts/Managers/Managers.cs
+++ Assets/_Scripts/Managers/Managers.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Managers 
+public class Managers
 {
     public static Managers Instance;
     public UIManager m_UIManager;
+    public CameraShake m_CameraShake;
     public Managers()
     {
         m_UIManager = new UIManager();
+        m_CameraShake = new CameraShake();
 
         if (Instance == null)
         {
@@ -18,11 +20,13 @@ public class Managers
 
     public void Start(GameObject parentCanvas)
     {
-        m_UIManager.Start(parentCanvas);      
+        m_UIManager.Start(parentCanvas);
+        m_CameraShake.Start(parentCanvas);
     }
 
     public void Update()
     {
         m_UIManager.Update();
+        m_CameraShake.Update();
     }
 }
