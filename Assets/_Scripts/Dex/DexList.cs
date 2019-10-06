7e3798820019b0fb984862f70613944017b9bc8d
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2019-10-06 03:39:04 +0300

    Added list scripts

---
+++ Assets/_Scripts/Dex/DexList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DexList
+{
+
+    public List<RecipeBase> dexList;
+
+    DexList()
+    {
+        dexList = new List<RecipeBase>();
+    }
+
+    #region Singleton things
+    private static DexList instance = null;
+    private static readonly object padlock = new object();
+
+    public static DexList Instance
+    {
+        get
+        {
+            lock (padlock)
+            {
+                if (instance == null)
+                {
+                    instance = new DexList();
+                }
+                return instance;
+            }
+        }
+    }
+    #endregion
+
+}
